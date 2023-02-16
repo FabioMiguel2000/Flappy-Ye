64b21f7a2252e9b77c86f1e8d7206ab7795bfc9d
Fab <fabiomiguelchenhuang@gmail.com>
2023-02-16 12:20:55 +0000

    added some physics to game object

--- Assets/YeScript.cs
+++ Assets/YeScript.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YeScript : MonoBehaviour
+public class yeScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidBody;
-    
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,8 @@ public class YeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) == true) {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
